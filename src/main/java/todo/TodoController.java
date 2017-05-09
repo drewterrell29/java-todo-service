0402da0033f5303d0a02ed04bdbac5e6e0c19646
Anthony Lee <anthony.m.lee@live.com>
2017-05-08 23:03:59 -0500

    Added logger to the controller

--- src/main/java/todo/TodoController.java
+++ src/main/java/todo/TodoController.java
@@ -1,8 +1,19 @@
 package todo;
 
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+
 /**
  * Created by antho on 5/8/2017.
  */
+@RestController
 public class TodoController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(TodoController.class);
+
 
 }
